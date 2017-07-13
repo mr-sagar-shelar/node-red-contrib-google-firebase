ec3cc70cca8f6b4ecdcf12be9a48f31cb2663ae3
FRANCISCO GUTIERREZ-MATURANA SANCHEZ <francisco.gutierrez@ingenieria.grupo.es>
2017-07-13 12:36:17 +0200

    Starting auth in node config

--- google-firebase/firebase_config.js
+++ google-firebase/firebase_config.js
@@ -1,62 +1,62 @@
-
 module.exports = function (RED) {
-  var firebase = require('firebase');
-  var events = require("events");
-  var path = require("path");
-  var https = require("follow-redirects").https;
-  var urllib = require("url");
+        var firebase = require('firebase');
+        var events = require("events");
+        var path = require("path");
+        var https = require("follow-redirects").https;
+        var urllib = require("url");
 
-  var connectionPool = function () {
-    var connections = [];
-    var isInitialized = false;
-    var mainApp = {};
+        var connectionPool = function () {
+        var connections = [];
+        var isInitialized = false;
+        var mainApp = {};
 
-    return {
-      get: function (config, configNodeID) {
-        if (isInitialized) {
-          return mainApp;
-        } else {
-          isInitialized = true;
-          var _emitter = new events.EventEmitter();
-          var _emit = function (a, b) {
-            if (this.lastEvent == a && this.lastEventData == b) {
-              return;
-            }
-            this.lastEvent = a;
-            this.lastEventData = b;
+        return {
+                get: function (config, configNodeID) {
+                        if (isInitialized) {
+                                return mainApp;
+                        }
 
-            _emitter.emit(a, b)
-          };
+                        isInitialized = true;
+                        var _emitter = new events.EventEmitter();
 
-          mainApp = {
-            fbApp: firebase.initializeApp(config),
-            on: function (a, b) { _emitter.on(a, b); },
-            once: function (a, b) { _emitter.once(a, b); },
-          };
-          return mainApp;
+                        var _emit = function (a, b) {
+                                if (this.lastEvent == a && this.lastEventData == b) {
+                                        return;
+                                }
+                                this.lastEvent = a;
+                                this.lastEventData = b;
+
+                                _emitter.emit(a, b)
+                        };
+
+                        mainApp = {
+                                fbApp: firebase.initializeApp(config),
+                                on: function (a, b) { _emitter.on(a, b); },
+                                once: function (a, b) { _emitter.once(a, b); },
+                        };
+                        return mainApp;
+                },
         }
-      },
-    }
-  } ();
+        } ();
 
-  function RemoteServerNode(n) {
-    RED.nodes.createNode(this, n);
+        function RemoteServerNode(n) {
+        RED.nodes.createNode(this, n);
 
-    this.databaseUrl = "https://" + n.databaseUrl + ".firebaseio.com";
-    this.authDomain = "https://" + n.authDomain + ".firebaseapp.com";
-    this.apiKey = n.apiKey;
+        this.databaseUrl = "https://" + n.databaseUrl + ".firebaseio.com";
+        this.authDomain = "https://" + n.authDomain + ".firebaseapp.com";
+        this.apiKey = n.apiKey;
 
-    if (this.databaseUrl) {
-      var config = {
+        if (this.databaseUrl) {
+        var config = {
         apiKey: this.apiKey,
         authDomain: this.authDomain,
         databaseURL: this.databaseUrl
-      };
-      this.fbConfig = connectionPool.get(config, this.id);
-    } else {
-      this.log('Firebase Not configured!!');
-    }
-  }
+        };
+        this.fbConfig = connectionPool.get(config, this.id);
+        } else {
+        this.log('Firebase Not configured!!');
+        }
+        }
 
-  RED.nodes.registerType("google-firebase-config", RemoteServerNode);
+        RED.nodes.registerType("google-firebase-config", RemoteServerNode);
 };
